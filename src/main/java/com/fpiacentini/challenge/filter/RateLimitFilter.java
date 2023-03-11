6214bb4bb0a34e764f5db98ccd3d513780fa1221
Federico Piacentini <fpiacentini@mulesoft.com>
2023-03-11 12:41:17 -0300

    chore(ratelimit): implement ratelimiting using bucket4j and a filter (#3)

---
+++ src/main/java/com/fpiacentini/challenge/filter/RateLimitFilter.java
@@ -0,0 +1,35 @@
+package com.fpiacentini.challenge.filter;
+
+import com.fpiacentini.challenge.ratelimit.BucketManager;
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class RateLimitFilter extends OncePerRequestFilter {
+
+    @Autowired
+    BucketManager bucketManager;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        if (!bucketManager.tryConsume()) {
+            response.setStatus(HttpStatus.TOO_MANY_REQUESTS.value());
+            response.setContentType("text/plain");
+            response.getWriter().append("Too many requests");
+            return;
+        }
+        filterChain.doFilter(request, response);
+    }
+}
