859bfd2851332e63bdb354e8851b838a9ed19f0e
fpiacentini <fpiacentini@mulesoft.com>
2023-03-12 12:51:06 -0300

    chore(test): add tests for controller

--- src/main/java/com/fpiacentini/challenge/controller/ChallengeController.java
+++ src/main/java/com/fpiacentini/challenge/controller/ChallengeController.java
@@ -25,7 +25,7 @@ public class ChallengeController {
     @PostMapping(path = "",
             consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Result> create(@RequestBody NumbersToAdd numbersToAdd) {
+    public ResponseEntity<Result> calculateResult(@RequestBody NumbersToAdd numbersToAdd) {
         Result result = calculationService.addNumbersAndApplyPercentage(numbersToAdd);
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
