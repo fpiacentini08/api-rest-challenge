1d938f323995f19516ffb464be3e06dfadd90024
fpiacentini <fpiacentini@mulesoft.com>
2023-03-14 19:09:06 -0300

    feat(docker): run the java api and db with docker compose

--- scripts/database/start-db.sh
+++ scripts/database/start-db.sh
@@ -2,12 +2,13 @@
 
 function delete_container_and_volume() {
         docker rm -f scripts_db_1
+        docker rm -f scripts_api_1
         docker volume rm scripts_db
         echo "Deleted container and volume."
 }
 
 delete_container_and_volume
-docker-compose --file ./scripts/docker-compose-psql.yml up
+docker-compose --file ./scripts/docker-compose-psql.yml --env-file ../../src/main/resources/application-default.properties up
 
 
 
